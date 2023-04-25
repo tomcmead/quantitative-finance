884f2fb417391caef90d85a769215836690496ee
Tom <tomcmead0@gmail.com>
2023-04-25 22:26:38 +0100

    add get historical stocks price data

--- quantitative_analysis/main.py
+++ quantitative_analysis/main.py
@@ -1,7 +1,9 @@
 import quantitative_analysis as qa
+import matplotlib.pyplot as plt
 
 def main():
-    quantAnalysis = qa.QuantativeAnalysis()
+    quant_analysis = qa.QuantativeAnalysis()
+    hist_stock_price = quant_analysis.get_hist_stock_prices("MSFT", "GOOG")
     return
 
 if __name__ == "__main__":
--- quantitative_analysis/quantitative_analysis.py
+++ quantitative_analysis/quantitative_analysis.py
@@ -1,5 +1,22 @@
 import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class QuantativeAnalysis:
     def __init__(self):
         return
+    
+    def get_hist_stock_prices(self, *tickers: str) -> list[pd.DataFrame]:
+        market_data = self.__get_stock_data(tickers)
+        return [(stock.High+stock.Low)/2 for stock in market_data]
+        # High, Low, Open, Close, Volume, Dividends
+    
+    def __get_stock_data(self, tickers: list[str]) -> list[pd.DataFrame]:
+        market_data = []
+        for ticker in tickers:
+            try:
+                ticker_data = yf.Ticker(ticker)
+                market_data.append(ticker_data.history(period="1mo"))
+            except:
+                print(f"Error: {ticker} not found and excluded")
+        return market_data
