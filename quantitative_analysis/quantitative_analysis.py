ffc87686f1cd57d86a67ab5dbf885e15e8bb32d6
tom <tomcmead0@gmail.com>
2023-05-03 23:03:03 +0100

    price, volume and dividends functions return pandas dataframe

--- quantitative_analysis/quantitative_analysis.py
+++ quantitative_analysis/quantitative_analysis.py
@@ -5,25 +5,45 @@ import matplotlib.pyplot as plt
 class QuantativeAnalysis:
     def __init__(self):
         return
-    
-    def get_hist_stock_prices(self, *tickers: str) -> list[pd.DataFrame]:
-        market_data = self.__get_stock_data(tickers)
-        return [(stock.High+stock.Low)/2 for stock in market_data]
-    
-    def get_hist_stock_volumes(self, *tickers: str) -> list[pd.DataFrame]:
-        market_data = self.__get_stock_data(tickers)
-        return [stock.Volume for stock in market_data]
-    
-    def get_hist_stock_dividends(self, *tickers: str) -> list[pd.DataFrame]:
-        market_data = self.__get_stock_data(tickers)
-        return [stock.Dividends for stock in market_data]
      
-    def __get_stock_data(self, tickers: list[str]) -> list[pd.DataFrame]:
-        market_data = []
+    def get_hist_stock_prices(self, *tickers: str) -> pd.DataFrame:
+        market_data = pd.Series([])
         for ticker in tickers:
             ticker_data = yf.Ticker(ticker).history(period="1mo")
             if ticker_data.empty:
                 print(f"Error: {ticker} not found and excluded")
             else:
-                market_data.append(ticker_data)
+                if market_data.empty:
+                    market_data = ticker_data.Open.rename(ticker)             
+                else:
+                    market_data = pd.concat([market_data, ticker_data.Open], axis=1)
+                    market_data.rename(columns={"Open": ticker}, inplace=True)   
+        return market_data
+
+    def get_hist_stock_volumes(self, *tickers: str) -> pd.DataFrame:
+        market_data = pd.Series([])
+        for ticker in tickers:
+            ticker_data = yf.Ticker(ticker).history(period="1mo")
+            if ticker_data.empty:
+                print(f"Error: {ticker} not found and excluded")
+            else:
+                if market_data.empty:
+                    market_data = ticker_data.Volume.rename(ticker)             
+                else:
+                    market_data = pd.concat([market_data, ticker_data.Volume], axis=1)
+                    market_data.rename(columns={"Volume": ticker}, inplace=True)   
+        return market_data
+    
+    def get_hist_stock_dividends(self, *tickers: str) -> pd.DataFrame:
+        market_data = pd.Series([])
+        for ticker in tickers:
+            ticker_data = yf.Ticker(ticker).history(period="1mo")
+            if ticker_data.empty:
+                print(f"Error: {ticker} not found and excluded")
+            else:
+                if market_data.empty:
+                    market_data = ticker_data.Dividends.rename(ticker)             
+                else:
+                    market_data = pd.concat([market_data, ticker_data.Dividends], axis=1)
+                    market_data.rename(columns={"Dividends": ticker}, inplace=True)   
         return market_data
--- tests/test_quantitative_analysis.py
+++ tests/test_quantitative_analysis.py
@@ -4,18 +4,33 @@ from quantitative_analysis import quantitative_analysis as qa
 
 def test_get_hist_stock_prices():
     quant_analysis = qa.QuantativeAnalysis()
-    assert type(quant_analysis.get_hist_stock_prices("MSFT", "GOOG")[0])==pd.Series
-    assert type(quant_analysis.get_hist_stock_prices("MSFT", "GOOG"))==list
-    assert quant_analysis.get_hist_stock_prices("Not_Real", "Fake_Stock")==[]
+
+    stock_prices = quant_analysis.get_hist_stock_prices("MSFT", "GOOG")
+    assert type(stock_prices)==pd.DataFrame    
+    assert list(stock_prices.columns)==["MSFT", "GOOG"]
+
+    stock_prices = quant_analysis.get_hist_stock_prices("FAKE_STOCK", "UNKOWN_STOCK")
+    assert type(stock_prices)==pd.Series    
+    assert stock_prices.empty
 
 def test_get_hist_stock_volumes():
     quant_analysis = qa.QuantativeAnalysis()
-    assert type(quant_analysis.get_hist_stock_volumes("MSFT", "GOOG")[0])==pd.Series
-    assert type(quant_analysis.get_hist_stock_volumes("MSFT", "GOOG"))==list
-    assert quant_analysis.get_hist_stock_volumes("Not_Real", "Fake_Stock")==[]   
+
+    stock_volumes = quant_analysis.get_hist_stock_volumes("MSFT", "GOOG")
+    assert type(stock_volumes)==pd.DataFrame    
+    assert list(stock_volumes.columns)==["MSFT", "GOOG"]
+
+    stock_volumes = quant_analysis.get_hist_stock_volumes("FAKE_STOCK", "UNKOWN_STOCK")
+    assert type(stock_volumes)==pd.Series    
+    assert stock_volumes.empty
 
 def test_get_hist_stock_dividends():
     quant_analysis = qa.QuantativeAnalysis()
-    assert type(quant_analysis.get_hist_stock_dividends("MSFT", "GOOG")[0])==pd.Series
-    assert type(quant_analysis.get_hist_stock_dividends("MSFT", "GOOG"))==list
-    assert quant_analysis.get_hist_stock_dividends("Not_Real", "Fake_Stock")==[]   
+
+    stock_dividends = quant_analysis.get_hist_stock_dividends("MSFT", "GOOG")
+    assert type(stock_dividends)==pd.DataFrame    
+    assert list(stock_dividends.columns)==["MSFT", "GOOG"]
+
+    stock_dividends = quant_analysis.get_hist_stock_dividends("FAKE_STOCK", "UNKOWN_STOCK")
+    assert type(stock_dividends)==pd.Series    
+    assert stock_dividends.empty
