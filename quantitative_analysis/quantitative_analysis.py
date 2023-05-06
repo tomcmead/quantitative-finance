20b0d668405fd5bc4ae682a8bc2b2822428bdcd5
Tom <tomcmead0@gmail.com>
2023-05-06 17:01:23 +0100

    generate basic html report for stock price, volume and dividends graphs with no labelling

--- quantitative_analysis/quantitative_analysis.py
+++ quantitative_analysis/quantitative_analysis.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 
 class QuantativeAnalysis:
     def __init__(self):
@@ -9,7 +11,7 @@ class QuantativeAnalysis:
     def get_hist_stock_prices(self, *tickers:str) -> pd.DataFrame:
         market_data = pd.Series([])
         for ticker in tickers:
-            ticker_data = yf.Ticker(ticker).history(period="1mo")
+            ticker_data = yf.Ticker(ticker).history(period="5y")
             if ticker_data.empty:
                 print(f"Error: {ticker} not found and excluded")
             else:
@@ -23,7 +25,7 @@ class QuantativeAnalysis:
     def get_hist_stock_volumes(self, *tickers:str) -> pd.DataFrame:
         market_data = pd.Series([])
         for ticker in tickers:
-            ticker_data = yf.Ticker(ticker).history(period="1mo")
+            ticker_data = yf.Ticker(ticker).history(period="5y")
             if ticker_data.empty:
                 print(f"Error: {ticker} not found and excluded")
             else:
@@ -37,7 +39,7 @@ class QuantativeAnalysis:
     def get_hist_stock_dividends(self, *tickers:str) -> pd.DataFrame:
         market_data = pd.Series([])
         for ticker in tickers:
-            ticker_data = yf.Ticker(ticker).history(period="1mo")
+            ticker_data = yf.Ticker(ticker).history(period="5y")
             if ticker_data.empty:
                 print(f"Error: {ticker} not found and excluded")
             else:
@@ -52,3 +54,24 @@ class QuantativeAnalysis:
         if(not market_data.empty):
             plt.plot(market_data)
             plt.show()
+
+    def generate_market_report(self, *tickers:str) -> None:
+        stock_prices = self.get_hist_stock_prices(*tickers)
+        stock_volumes = self.get_hist_stock_volumes(*tickers)
+        stock_dividends = self.get_hist_stock_dividends(*tickers)
+
+        html = '<!DOCTYPE html><html><body>'
+        html += '<img src=\'data:image/png;base64,{}\'>'.format(self.__generate_market_figure(stock_prices))
+        html += '<img src=\'data:image/png;base64,{}\'>'.format(self.__generate_market_figure(stock_volumes))
+        html += '<img src=\'data:image/png;base64,{}\'>'.format(self.__generate_market_figure(stock_dividends))
+        html += '</body></html>'
+        with open('index.html', 'w') as f:
+            f.write(html)
+
+    def __generate_market_figure(self, market_data:pd.DataFrame) -> str:
+        fig = plt.figure()
+        plt.plot(market_data)
+        tmpfile = BytesIO()
+        fig.savefig(tmpfile, format='png')
+        encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+        return encoded
